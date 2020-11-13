7fc23af06b105b83e6db4aa3bac4a89d208a34a3
Adil Botabekov <botabekov.adil@gmail.com>
2020-11-13 14:12:47 +0600

    extractor working v1

--- data/test.py
+++ data/test.py
@@ -1,2 +1,5 @@
 def addTwoNumbers(x: int, y: int) -> int:
     return x + y
+
+def decrementNumber(x: int) -> int:
+    return x - 1
--- extractor.py
+++ extractor.py
@@ -3,25 +3,69 @@ import ast
 # from nltk.tokenize import sent_tokenize, word_tokenize
 from pprint import pprint
 
-def build_tree(filename):
+def process_file(filename):
     with open(filename, "r") as source:
         tree = ast.parse(source.read())
     
-    return tree
+    funcDefExtractor = FuncDefExtractor()
+    funcDefExtractor.visit(tree)
 
-# class Analyzer(ast.NodeVisitor):
-#     def __init__(self):
-#             self.stats = {"import": [], "from": []}
+    funcDefs = funcDefExtractor.returnStats()
+
+    output = []
+
+    for funcDef in funcDefs:
+        visitor = Visitor()
+        visitor.visit(funcDef)
+        stats = visitor.returnStats()
+        outObject = {
+            "name": funcDef.name, 
+            "returnTypes": ast.dump(funcDef.returns),
+            "args": stats["args"],
+            "returnStatement": stats["returnStatement"]
+            }
+        
+        output.append(outObject)
+        
+
+    return output
+    
+
+class FuncDefExtractor(ast.NodeVisitor):
+    def __init__(self):
+            self.stats = []
+
+    def returnStats(self):
+        return self.stats
+
+    def visit_FunctionDef(self, node):
+        self.stats.append(node)
+        self.generic_visit(node)
+
+    
 
 
+class Visitor(ast.NodeVisitor):
+    def __init__(self):
+            self.stats = { "args": [], "returnStatement": [] }
 
-#     def report(self):
-#         pprint(self.stats)
+    def returnStats(self):
+        return self.stats
+
+    def visit_arg(self, node):
+        self.stats["args"].append(ast.dump(node))
+        self.generic_visit(node)
+
+    def visit_Return(self, node):
+        self.stats["returnStatement"].append(ast.dump(node))
+        self.generic_visit(node)
+
 
 
 def main():
-    tree = build_tree("./data/test.py")
-    print(ast.dump(tree))
+    output = process_file("./data/test.py")
+    print(output)
+
 
 if __name__ == "__main__":
     main()
