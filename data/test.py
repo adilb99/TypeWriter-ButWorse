aae2ce1c1da5af026ba49b243db8a33631ba2f6e
Adil Botabekov <botabekov.adil@gmail.com>
2020-11-13 12:49:30 +0600

    ast test

---
+++ data/test.py
@@ -0,0 +1,2 @@
+def addTwoNumbers(x: int, y: int) -> int:
+    return x + y
--- extractor.py
+++ extractor.py
@@ -1,3 +1,27 @@
-import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+# import nltk
 import ast
+# from nltk.tokenize import sent_tokenize, word_tokenize
+from pprint import pprint
+
+def build_tree(filename):
+    with open(filename, "r") as source:
+        tree = ast.parse(source.read())
+    
+    return tree
+
+# class Analyzer(ast.NodeVisitor):
+#     def __init__(self):
+#             self.stats = {"import": [], "from": []}
+
+
+
+#     def report(self):
+#         pprint(self.stats)
+
+
+def main():
+    tree = build_tree("./data/test.py")
+    print(ast.dump(tree))
+
+if __name__ == "__main__":
+    main()
